0a58b68bfe4ba3137e308bdfc5e42d086f9e683d
Guillaume Claret <dev@clarus.me>
2015-06-30 11:57:10 +0200

    Implicit arguments

--- src/All.v
+++ src/All.v
@@ -2,4 +2,6 @@ Module Monad.
   Record t (M : Type -> Type) : Type := New {
     ret : forall {A : Type}, A -> M A;
     bind : forall {A B : Type}, M A -> (A -> M B) -> M B }.
+  Arguments ret {M} _ {A} _.
+  Arguments bind {M} _ {A B} _ _.
 End Monad.
